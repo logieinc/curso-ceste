115a73081769cf57daafa268449acfb5e7662c97
Fabian Giordano <>
2024-03-07 14:13:57 -0300

    - Correccion en clase GetSecretWrapper para determinar clave - valor

--- Taller-boto3/secretsmanager/ejemplo-01.py
+++ Taller-boto3/secretsmanager/ejemplo-01.py
@@ -1,13 +1,15 @@
 """
     Obtener secrets desde AWS
 """
+import json
 import logging
 from venv import logger
 
 import boto3
 
-SECRET_NAME = "test/twitter"
+SECRET_NAME = "secreto/database"
 REGION_NAME = "us-east-1"
+
 class GetSecretWrapper:
     def __init__(self, secretsmanager_client):
         self.client = secretsmanager_client
@@ -34,6 +36,31 @@ class GetSecretWrapper:
         except Exception as e:
             raise e
 
+    def get_clave_valor(self, secret_name):
+        """
+        Recupera secretos individuales de AWS Secrets Manager mediante la API get_secret_value.
+        Esta función supone que la pila mencionada en el código fuente README se ha implementado correctamente.
+        Esta pila incluye 7 secretos, todos los cuales tienen nombres que comienzan con "mySecret".
+
+        :param secret_name: el nombre del secreto obtenido.
+        : escriba nombre_secreto: cadena
+        """
+        try:
+            get_secret_value_response = self.client.get_secret_value(
+                SecretId=secret_name
+            )
+            logging.info("Secreto recuperado con éxito.")
+            data = json.loads(get_secret_value_response["SecretString"])
+            for clave, valor in data.items():
+                print(f"Clave: {clave}, Valor: {valor}")
+            return clave, valor
+        except self.client.exceptions.ResourceNotFoundException:
+            msg = f"El secreto solicitado {secret_name} no fue encontrado."
+            logger.info(msg)
+            return msg
+        except Exception as e:
+            raise e
+
 
 if __name__ == "__main__":
     try:
@@ -43,7 +70,10 @@ if __name__ == "__main__":
             region_name=REGION_NAME
         )
         print("")
+        print("-" * 80)
         print(GetSecretWrapper(client).get_secret(SECRET_NAME))
-
+        print("-" * 80)
+        print(GetSecretWrapper(client).get_clave_valor(SECRET_NAME))
+        print("-" * 80)
     except Exception as e:
         logger.error(f"Un error desconocido ocurrió: {str(e)}.")
